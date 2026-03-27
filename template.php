fa148313b87cdb11cd683a9eb857cd300243fae9
mollysych <148881266+mollysych@users.noreply.github.com>
2026-03-27 10:09:05 +0700

    updated

--- template.php
+++ template.php
@@ -1,31 +1,39 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-<div class="news-list">
-    <?foreach($arResult["ITEMS"] as $arItem):?>
+<div id="barba-wrapper">
+    <div class="article-list">
 
-        <div class="news-card">
-            <a class="news-card__link" href="<?=$arItem["DETAIL_PAGE_URL"] ?: '#'?>">
+        <?foreach($arResult["ITEMS"] as $arItem):?>
 
-                <?if(!empty($arItem["PREVIEW_PICTURE"]["SRC"])):?>
-                    <div class="news-card__image">
-                        <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["NAME"]?>">
+            <a class="article-item article-list__item"
+               href="<?=$arItem["DETAIL_PAGE_URL"] ?: '#'?>"
+               data-anim="anim-3">
+
+                <div class="article-item__background">
+                    <?php
+                    // picture from infoblock or default
+                    $img = !empty($arItem["PREVIEW_PICTURE"]["SRC"])
+                        ? $arItem["PREVIEW_PICTURE"]["SRC"]
+                        : $templateFolder."/images/article-item-bg-1.jpg";
+                    ?>
+                    <img src="<?=$img?>" alt="<?=htmlspecialchars($arItem["NAME"])?>">
+                </div>
+
+                <div class="article-item__wrapper">
+
+                    <div class="article-item__title">
+                        <?=htmlspecialchars($arItem["NAME"])?>
                     </div>
-                <?endif;?>
 
-                <div class="news-card__body">
-                    <div class="news-card__title">
-                        <?=$arItem["NAME"]?>
+                    <div class="article-item__content">
+                        <?=htmlspecialchars($arItem["PREVIEW_TEXT"])?>
                     </div>
 
-                    <?if(!empty($arItem["PREVIEW_TEXT"])):?>
-                        <div class="news-card__text">
-                            <?=$arItem["PREVIEW_TEXT"]?>
-                        </div>
-                    <?endif;?>
                 </div>
 
             </a>
-        </div>
 
-    <?endforeach;?>
+        <?endforeach;?>
+
+    </div>
 </div>
