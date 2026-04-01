ed3218e898e54cde348c185b885540cbf4fee654
mollysych <148881266+mollysych@users.noreply.github.com>
2026-04-01 18:24:03 +0700

    task_2 uploaded

--- template.php
+++ template.php
@@ -1,39 +1,66 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+?>
 
-<div id="barba-wrapper">
-    <div class="article-list">
+<div class="contact-form">
 
-        <?foreach($arResult["ITEMS"] as $arItem):?>
+    <div class="contact-form__head">
+        <div class="contact-form__head-title"><?= htmlspecialcharsbx($arResult['FORM_TITLE']) ?></div>
+        <?php if (!empty($arResult['FORM_DESCRIPTION'])): ?>
+            <div class="contact-form__head-text"><?= htmlspecialcharsbx($arResult['FORM_DESCRIPTION']) ?></div>
+        <?php endif; ?>
+    </div>
 
-            <a class="article-item article-list__item"
-               href="<?=$arItem["DETAIL_PAGE_URL"] ?: '#'?>"
-               data-anim="anim-3">
+    <?php if ($arResult['isFormErrors'] === 'Y'): ?>
+        <div class="contact-form__errors">
+            <?= $arResult['FORM_ERRORS_TEXT'] ?>
+        </div>
+    <?php endif; ?>
 
-                <div class="article-item__background">
-                    <?php
-                    // picture from infoblock or default
-                    $img = !empty($arItem["PREVIEW_PICTURE"]["SRC"])
-                        ? $arItem["PREVIEW_PICTURE"]["SRC"]
-                        : $templateFolder."/images/article-item-bg-1.jpg";
-                    ?>
-                    <img src="<?=$img?>" alt="<?=htmlspecialchars($arItem["NAME"])?>">
+    <?= $arResult['FORM_HEADER'] ?>
+
+    <div class="contact-form__form">
+
+        <div class="contact-form__form-inputs">
+            <?php foreach ($arResult['QUESTIONS'] as $fieldSid => $arQuestion): ?>
+                <div class="input contact-form__input">
+                    <label class="input__label">
+
+                        <div class="input__label-text">
+                            <?= htmlspecialcharsbx($arQuestion['CAPTION']) ?>
+                            <?php if ($arQuestion['REQUIRED'] === 'Y'): ?><?= $arResult['REQUIRED_SIGN'] ?><?php endif; ?>
+                        </div>
+
+                        <?php
+                        $html = $arQuestion['HTML_CODE'];
+                        $html = str_replace('<input', '<input class="input__input"', $html);
+                        $html = str_replace('<textarea', '<textarea class="input__input"', $html);
+                        ?>
+                        <?= $html ?>
+
+                        <?php if (!empty($arResult['FORM_ERRORS'][$fieldSid])): ?>
+                            <div class="input__notification">
+                                <?= htmlspecialcharsbx($arResult['FORM_ERRORS'][$fieldSid]) ?>
+                            </div>
+                        <?php endif; ?>
+
+                    </label>
                 </div>
+            <?php endforeach; ?>
+        </div>
 
-                <div class="article-item__wrapper">
+        <div class="contact-form__bottom">
+            <div class="contact-form__bottom-policy">
+                Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.
+            </div>
 
-                    <div class="article-item__title">
-                        <?=htmlspecialchars($arItem["NAME"])?>
-                    </div>
-
-                    <div class="article-item__content">
-                        <?=htmlspecialchars($arItem["PREVIEW_TEXT"])?>
-                    </div>
-
-                </div>
-
-            </a>
-
-        <?endforeach;?>
+            <input type="submit" name="web_form_submit"
+                   class="form-button contact-form__bottom-button"
+                   value="<?= htmlspecialcharsbx(trim($arResult['arForm']['BUTTON']) === '' ? GetMessage('FORM_ADD') : $arResult['arForm']['BUTTON']); ?>" />
+        </div>
 
     </div>
+
+    <?= $arResult['FORM_FOOTER'] ?>
+
 </div>
